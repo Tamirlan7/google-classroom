f8045ccf497f0cab6da8f228289188bbad24f605
Tamirlan Zhukasaev <zukasaevtamirlan1@gmail.com>
2023-02-21 22:52:24 +0600

    now google account finished for sure(i hope)

--- frontend/src/pages/Profile/ProfileForms/ProfileFormHeader.tsx
+++ frontend/src/pages/Profile/ProfileForms/ProfileFormHeader.tsx
@@ -5,6 +5,8 @@ import { ReactComponent as QuestionMark } from '../../../assets/icons/question-m
 import { ReactComponent as ArrowLeft } from '../../../assets/icons/arrow-left.svg'
 import './ProfileForm.css';
 import { useTypedSelector } from '../../../hooks/redux';
+import Settings from '../../../components/Settings/Settings';
+import { useOutsideClick } from '../../../hooks/useClick';
 
 interface ProfileFormHeaderProps extends React.PropsWithChildren {
     title: string
@@ -13,6 +15,15 @@ interface ProfileFormHeaderProps extends React.PropsWithChildren {
 const ProfileFormHeader: React.FC<ProfileFormHeaderProps> = ({ title }) => {
     const [isScrolling, setIsScrolling] = useState<boolean>(false);
     const avatarPath = useTypedSelector(state => state.profile.profile.avatar);
+    const [isSettingsActive, setIsSettingsActive] = useState<boolean>(false)
+
+    function closeSettings() {
+        setIsSettingsActive(false)
+    }
+
+    const settingsRef = React.useRef<HTMLDivElement>(null)
+    useOutsideClick(settingsRef, closeSettings);
+
     console.log('ProfileFormHeader render');
     function addBoxShadow() {
         if(window.scrollY > 64) 
@@ -26,7 +37,7 @@ const ProfileFormHeader: React.FC<ProfileFormHeaderProps> = ({ title }) => {
     return (
         <header className={isScrolling ? 'profile-header header-shadow field-header' : 'profile-header field-header'}>
             <div className='inner__header field-inner__header'>
-                <div className="logo field-logo">
+                <div className="logo field-logo" >
                     <Link to="/" className="logo-link">
                         <span className="logo-icon" role={"button"}>
                         </span>
@@ -38,7 +49,10 @@ const ProfileFormHeader: React.FC<ProfileFormHeaderProps> = ({ title }) => {
                 <div className="icons">
                     <span><QuestionMark /></span>
                     <span><DottedMenu /></span>
-                    <div className="icons-avatar"><img src={`${process.env.REACT_APP_API_URL}${avatarPath}`} alt="avatar" /></div>
+                    <div className="icons-avatar" onClick={() => setIsSettingsActive(true)}>
+                        <img src={`${process.env.REACT_APP_API_URL}${avatarPath}`} alt="avatar" />
+                    </div>
+                    {isSettingsActive && <Settings settingsRef={settingsRef} />}
                 </div>
             </div>
             <div className='field-title'>
